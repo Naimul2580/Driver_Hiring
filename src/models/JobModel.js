30bfaff4c3d78423f80c13b6f69f85da7be33b73
Naimul2580 <21201142@uap-bd.edu>
2025-03-07 21:54:27 +0600

    Job Model

---
+++ src/models/JobModel.js
@@ -0,0 +1,15 @@
+// for posting job
+
+const mongoose = require("mongoose");
+
+const JobSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    postedBy: { type: mongoose.Schema.Types.ObjectId, ref: "admins", required: true },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const JobModel = mongoose.model("jobs", JobSchema);
+module.exports = JobModel;
