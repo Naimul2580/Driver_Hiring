4706e19b142db38555a0293e1fb9e49adb985e04
Naimul2580 <21201142@uap-bd.edu>
2025-04-03 21:27:46 +0600

    Admin Routing Completed

--- Back_End/src/routes/api.js
+++ Back_End/src/routes/api.js
@@ -1,6 +1,17 @@
 const express = require("express")
 
+const AdminController = require('../controllers/AdminController')
+const CustomerSupportController = require('../controllers/CustomerSupportController')
+
 const router = express.Router()
+
+//Admin
+router.get('/Users',AdminController.getAllUsers) // Route to get all users
+router.get('/Drivers',AdminController.getAllDrivers) // Route to get all drivers
+router.patch("/users/:id/suspend", AdminController.suspendUser) // Route to suspend a user
+router.patch("/drivers/:id/approve", AdminController.approveDriver) // Route to approve a driver
+
+//CustomerSupport
 
 
 
