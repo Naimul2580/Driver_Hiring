a90f91e2cc40eea8a37f2611b84aeab30f7c2132
Naimul2580 <21201142@uap-bd.edu>
2025-04-03 21:43:22 +0600

    Job Routing Created

--- Back_End/src/routes/api.js
+++ Back_End/src/routes/api.js
@@ -6,15 +6,22 @@ const CustomerSupportController = require('../controllers/CustomerSupportControl
 const router = express.Router()
 
 //Admin
-router.get('/Users',AdminController.getAllUsers) // Route to get all users
-router.get('/Drivers',AdminController.getAllDrivers) // Route to get all drivers
-router.patch("/users/:id/suspend", AdminController.suspendUser) // Route to suspend a user
-router.patch("/drivers/:id/approve", AdminController.approveDriver) // Route to approve a driver
+router.get('/Users',AdminController.getAllUsers); // Route to get all users
+router.get('/Drivers',AdminController.getAllDrivers); // Route to get all drivers
+router.patch("/users/:id/suspend", AdminController.suspendUser); // Route to suspend a user
+router.patch("/drivers/:id/approve", AdminController.approveDriver); // Route to approve a driver
 
 //CustomerSupport
 router.post("/tickets", CustomerSupportController.createTicket); // Route to create a support ticket
-router.get("/tickets", CustomerSupportController.getTickets); // Route to get all support tickets
+router.get("/getTickets", CustomerSupportController.getTickets); // Route to get all support tickets
 router.patch("/tickets/:id/status", CustomerSupportController.updateTicketStatus); // Route to update the status of a support ticket
+
+//Job
+router.post("/create", JobController.createJob);
+router.get("/getJobs", JobController.getJobs);
+router.post("/:jobId/apply", JobController.applyForJob);
+router.delete("/:id", JobController.deleteJob);
+
 
 
 
