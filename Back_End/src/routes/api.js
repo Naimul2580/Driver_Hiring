5ed488779687853f57b24a8ba897c76bccb160d3
Naimul2580 <21201142@uap-bd.edu>
2025-04-03 21:47:50 +0600

    payment Controller Created

--- Back_End/src/routes/api.js
+++ Back_End/src/routes/api.js
@@ -2,6 +2,9 @@ const express = require("express")
 
 const AdminController = require('../controllers/AdminController')
 const CustomerSupportController = require('../controllers/CustomerSupportController')
+const JobController = require("../controllers/JobController");
+const PaymentController = require("../controllers/PaymentController");
+
 
 const router = express.Router()
 
@@ -17,12 +20,15 @@ router.get("/getTickets", CustomerSupportController.getTickets); // Route to get
 router.patch("/tickets/:id/status", CustomerSupportController.updateTicketStatus); // Route to update the status of a support ticket
 
 //Job
-router.post("/create", JobController.createJob);
-router.get("/getJobs", JobController.getJobs);
-router.post("/:jobId/apply", JobController.applyForJob);
-router.delete("/:id", JobController.deleteJob);
+router.post("/create", JobController.createJob); // Route to create a job
+router.get("/getJobs", JobController.getJobs); // Route to get all jobs
+router.post("/:jobId/apply", JobController.applyForJob); // Route to apply for a job
+router.delete("/:id", JobController.deleteJob); // Route to delete a job
 
-
+//Payment
+router.post("/initiate", PaymentController.initiatePayment); // Route to initiate payment
+router.get("/:paymentId/verify", PaymentController.verifyPayment); // Route to verify payment
+router.get("/history/:userId", PaymentController.getPaymentHistory); // Route to get payment history
 
 
 
