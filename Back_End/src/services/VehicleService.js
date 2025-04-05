3c4c3f5b3a9d089d753116c3c1d8780975f5317a
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-05 22:09:16 +0600

    Vehicle Service

---
+++ Back_End/src/services/VehicleService.js
@@ -0,0 +1,21 @@
+const VehicleModel = require("../models/VehicleModel");
+
+const addVehicleService = async (vehicleData) => {
+};
+
+const updateVehicleService = async (vehicleId, updateData) => {
+};
+
+const getVehicleService = async (vehicleId) => {
+  
+};
+
+const deleteVehicleService = async (vehicleId) => {
+};
+
+module.exports = {
+  addVehicleService,
+  updateVehicleService,
+  getVehicleService,
+  deleteVehicleService,
+};
