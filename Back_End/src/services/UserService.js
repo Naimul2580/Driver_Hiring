110dfa006e3e08433e1e984918f467d8250ad741
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-05 22:04:59 +0600

    User Service

---
+++ Back_End/src/services/UserService.js
@@ -0,0 +1,20 @@
+const UserModel = require("../models/UserModel");
+
+const getUserProfileService = async (userId) => {
+};
+
+const updateUserProfileService = async (userId, updateData) => {
+};
+
+const deleteUserAccountService = async (userId) => {
+};
+
+const getAllUsersService = async () => {
+};
+
+module.exports = {
+  getUserProfileService,
+  updateUserProfileService,
+  deleteUserAccountService,
+  getAllUsersService,
+};
