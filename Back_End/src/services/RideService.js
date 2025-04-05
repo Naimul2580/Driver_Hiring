4085e16c064020ec8ccc1797e9aac9d5523d3cba
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-05 22:03:27 +0600

    Ride Service

---
+++ Back_End/src/services/RideService.js
@@ -0,0 +1,24 @@
+const RideModel = require("../models/RideModel");
+
+const bookRideService = async (rideData) => {
+};
+
+const acceptRideService = async (rideId, driverId) => {
+};
+
+const updateRideStatusService = async (rideId, status) => {
+};
+
+const cancelRideService = async (rideId) => {
+};
+
+const getRideHistoryService = async (userId) => {
+};
+
+module.exports = {
+  bookRideService,
+  acceptRideService,
+  updateRideStatusService,
+  cancelRideService,
+  getRideHistoryService,
+};
