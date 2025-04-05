7f51ce12ec15fcba4a5d8addc5f93936bfa6cefe
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-05 22:10:42 +0600

    Verification Service

---
+++ Back_End/src/services/VerificationService.js
@@ -0,0 +1,16 @@
+const VerificationModel = require("../models/VerificationModel");
+
+const submitDocumentService = async (documentData) => {
+};
+
+const approveDocumentService = async (documentId) => {
+};
+
+const getPendingVerificationsService = async () => {
+};
+
+module.exports = {
+  submitDocumentService,
+  approveDocumentService,
+  getPendingVerificationsService,
+};
